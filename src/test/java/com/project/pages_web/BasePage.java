7ea8e84ec9be53d868f384f1bb1946a034b8072f
suat525255 <suataydin5255@gmail.com>
2025-01-23 12:25:48 +0300

    remove feature

--- src/test/java/com/project/pages_web/BasePage.java
+++ src/test/java/com/project/pages_web/BasePage.java
@@ -20,45 +20,7 @@ public abstract class BasePage {
     WebDriver driver = Driver.get();
 
 
-    public void navigateToModule(String tab, String subTab, String module) {
 
-        String tabLocator = "//span[text()='" + tab + "']";
-        String subTabLocator = "//a[text()='" + subTab + "']";
-        String moduleLocator = "//*[contains(@class, 'ChildMenuItems')]//a[text()='" + module + "']";
-
-
-        WebElement moduleElement;
-
-        try {
-            BrowserUtils.waitFor(1);
-            BrowserUtils.hover(driver.findElement(By.xpath(tabLocator)));//for stale element
-            BrowserUtils.waitFor(1);
-            BrowserUtils.hover(driver.findElement(By.xpath(subTabLocator)));
-            BrowserUtils.waitFor(1);
-        } catch (Exception e) {
-            BrowserUtils.hover(nonPremiumLogo);
-            BrowserUtils.waitFor(1);
-            BrowserUtils.hover(driver.findElement(By.xpath(tabLocator)));
-            BrowserUtils.waitFor(1);
-            BrowserUtils.hover(driver.findElement(By.xpath(subTabLocator)));
-            BrowserUtils.waitFor(1);
-        }
-
-
-        try {
-            BrowserUtils.waitForPresenceOfElement(By.xpath(moduleLocator), Duration.ofSeconds(5));
-            BrowserUtils.waitForVisibility(By.xpath(moduleLocator), 5);
-            moduleElement = Driver.get().findElement(By.xpath(moduleLocator));
-            BrowserUtils.scrollToElement(moduleElement);
-            moduleElement.click();
-        } catch (Exception e) {
-            System.out.println("3");
-            BrowserUtils.clickWithTimeOut(Driver.get().findElement(By.xpath(moduleLocator)),  5);
-
-        }
-
-        Assert.assertTrue("module contains url", driver.getCurrentUrl().contains(module.toLowerCase()));
-    }
 
 }
 
--- src/test/java/com/project/step_definitions/SearchDefs.java
+++ src/test/java/com/project/step_definitions/SearchDefs.java
@@ -21,50 +21,5 @@ public class SearchDefs {
     MainPage page = new MainPage();
     SearchPage searchPage = new SearchPage();
 
-//    @Given("search {string} product")
-//    public void searchProduct(String product) {
-//        searchPage.searchProduct(product);
-//    }
 
-    @Then("search result should be {string}")
-    public void searchResultShouldBe(String product) {
-        Assert.assertEquals("verify search result title" ,searchPage.searchResultKeyword.getText() , product);
-    }
-
-
-    @Given("the user navigates to {string} to {string}")
-    public void theUserNavigatesToTo(String arg0, String arg1) {
-        
-    }
-
-    @Given("the user navigates to {string} to {string} to {string}")
-    public void theUserNavigatesToToTo(String tab, String subTab, String module) {
-        page.navigateToModule(tab, subTab, module);
-    }
-
-    @When("select title {string} filter {string}")
-    public void selectTitleFilter(String filterTitle, String filterName) {
-        searchPage.selectFilter(filterTitle, filterName);
-    }
-
-    @When("click highest price product")
-    public void click_highest_price_product() {
-        searchPage.highestPrice();
-
-    }
-
-
-    @Then("selected filter should be {int}")
-    public void selectedFilterShouldBe(int filterSize) {
-        Assert.assertEquals(filterSize, searchPage.selectedFilterLocator.size());
-    }
-
-    @And("selected filter name should be same")
-    public void selectedFilterNameShouldBeSame(List<String> filter) {
-        List<String> actual = BrowserUtils.createList(searchPage.selectedFilterLocator);
-        Set<String> actualSet = new HashSet<>(actual);
-        Set<String> filterSet = new HashSet<>(filter);
-
-        Assert.assertTrue("Actual list does not match filter list.", actualSet.containsAll(filterSet) && filterSet.containsAll(actualSet));
-    }
 }
